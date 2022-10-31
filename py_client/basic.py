4d440b65940fc856f3995eaee44251e239213efa
mochaji27 <muhamadaji1020@gmail.com>
2022-11-01 04:59:29 +0700

    Add first API

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,3 +1,8 @@
 import requests
 
-endpoint = "https://httpbin.org"
+endpoint = "http://127.0.0.1:8000/api"
+
+get_response = requests.get(endpoint, json={"query" : "Hellow World"})
+print(get_response.text)
+print(get_response.json)
+print(get_response.status_code)
