943ad8de8e29d5b88989d7c4153c3c7c9b3a62ce
mochaji27 <muhamadaji1020@gmail.com>
2022-11-03 11:26:22 +0700

    Update

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,24 @@
-from django.http import JsonResponse
+from email import header
+import json
 
-# Create your views here.
+from django.forms.models import model_to_dict
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from products.models import Product
+from products.seriliazers import ProductSeriliazer
+
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"Message" : "Hi there, this is your dhjango API response!!"})
+    """
+    DRF API View
+    """
+    instance = Product.objects.all().order_by("?").first()
+    data = {}
+    if instance:
+        # data = model_to_dict(instance, fields=['id', 'title', 'price', 'sale_price'])
+        data = ProductSeriliazer(instance).data
+    return Response(data)
+    #     json_data_str = json.dumps(data)
+    # return HttpResponse(json_data_str, headers = {'content-type' : 'application/json'})
